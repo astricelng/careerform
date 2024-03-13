c529b7fe78abb9f3594db0bfda0920992d3041dc
Astrid Narvaez <astricelng@gmail.com>
2024-03-13 14:29:01 -0600

    test create file

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('career', function(){
-    app('files')->put(base_path('resources/blueprints/forms/career.yaml'), 'hola');
-    return 'Hello from the career form package';
+    app('files')->put(resource_path('blueprints/forms/career.yaml'), 'hola');
+    return 'Hello from the career form package 1';
 });
 
 
