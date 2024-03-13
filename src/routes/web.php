e14d72629f5efabda102e183ce274ddcb14b253f
Astrid Narvaez <astricelng@gmail.com>
2024-03-13 14:57:41 -0600

    test create file

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Statamic\Facades\AssetContainer;
+use Statamic\Facades\Form;
+
 Route::get('career', function(){
-    app('files')->put(resource_path('blueprints/forms/career.yaml'), 'hola');
-    return 'Hello from the career form package 1';
+
+    $files = AssetContainer::find('files');
+    $files->assetFolder('career')->save();
+
+    $careerForm = app('files')->get(base_path('resources/templates/forms/career.yaml'));
+    app('files')->put(base_path('resources/blueprints/forms/career.yaml'), $careerForm);
+
+    $form = Form::make()->handle('career');
+    $form
+        ->handle('career')
+        ->honeypot('fax')
+        ->title('Career')
+        ->save();
+
+    return 'Hello from the career form package 2';
 });
 
 
