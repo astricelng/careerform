419825660c5189ef54cda019e5eb570f3f9c54fb
Astrid Narvaez <astricelng@gmail.com>
2024-03-14 13:10:13 -0600

    create career form and collection

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use Statamic\Facades\AssetContainer;
 use Statamic\Facades\Form;
+use Statamic\Facades\Collection;
 
 Route::get('career', function(){
 
@@ -18,6 +19,16 @@ Route::get('career', function(){
         ->handle('career')
         ->honeypot('fax')
         ->title('Career')
+        ->save();
+
+    $collection = Collection::make('careers');
+    $collection
+        ->title('Careers')
+        ->dated(true)
+        ->template('default')
+        ->layout('layout')
+        ->revisionsEnabled(false)
+        ->sortDirection('asc')
         ->save();
 
     return 'Hello from the career form package 2';
